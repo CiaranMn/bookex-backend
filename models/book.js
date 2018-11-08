337d5e7822d84c6374280616ffdf1846a1df6efc
Ciaran M <cmorinan@gmail.com>
2018-11-08 16:24:59 +0000

    basic book query endpoint

--- models/book.js
+++ models/book.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const _ = require('lodash')
 
 const BookSchema = new mongoose.Schema({
   title: {
@@ -8,8 +9,6 @@ const BookSchema = new mongoose.Schema({
   }, author: {
     type: String,
     required: [true, 'Author must be supplied']
-  }, subtitle: {
-    type: String,
   }, description: {
     type: String
   }, published_at: {
@@ -18,11 +17,30 @@ const BookSchema = new mongoose.Schema({
     type: String
   }], ISBN_13: {
     type: Number,
+  }, google_id: {
+    type: Number,
   }, image: {
     type: String
   }
 })
 
+class BookClass {
+
+  static convertGoogleObject (obj) {
+    return {
+      title: _.get(obj, 'volumeInfo.title'),
+      author: _.get(obj, 'volumeInfo.authors[0]'),
+      description: _.get(obj, 'volumeInfo.description'),
+      published_at: _.get(obj, 'volumeInfo.publishedDate'),
+      categories: _.get(obj, 'volumeInfo.categories'),
+      ISBN_13: _.get(obj, 'volumeInfo.industryIdentifiers[0].identifier'),
+      google_id: _.get(obj, 'id'),
+      image: _.get(obj, 'volumeInfo.imageLinks.thumbnail')
+    }
+  }
+}
+
+BookSchema.loadClass(BookClass)
 const Book = mongoose.model('Book', BookSchema)
 
 module.exports = { Book }
--- server.js
+++ server.js
@@ -5,11 +5,14 @@ const port = process.env.PORT || 3000
 const {mongoose} = require('./db/mongoose')
 const {User} = require('./models/user')
 const {Book} = require('./models/book')
+const {BooksApi} = require('./adapters/google_books')
 
-app.get('/', (request, response) => {
-  console.log(request)
-  response.status(200).send('Hello World')
-}
-)
+app.get('/books', (request, response) => {
+  if (request.query.q) {
+    let start = request.query.start || 0
+    BooksApi.searchBooks(request.query.q, start)
+      .then(resp => response.send(resp))
+  } 
+})
 
 app.listen(port, () => console.log(`Server listening on port ${port}.`))
