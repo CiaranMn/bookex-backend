8aa934822f845b45df609b560721d435b036aa55
Ciaran M <cmorinan@gmail.com>
2018-11-09 09:47:36 +0000

    added cors

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = process.env.PORT || 3000
 
 const {mongoose} = require('./db/mongoose')
 const {User} = require('./models/user')
 const {Book} = require('./models/book')
 const {BooksApi} = require('./adapters/google_books')
+
+app.use(cors())
 
 app.get('/books', (request, response) => {
   if (request.query.q) {
